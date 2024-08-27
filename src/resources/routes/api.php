23fa88b9849260efcf2b5847a302cde6d1d0c3f9
4multimedia <biuro@4multimedia.pl>
2024-08-27 12:56:25 +0200

    1.0.1

--- src/resources/routes/api.php
+++ src/resources/routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('pl')->as('pl.')->namespace('Poland')->group(function() {
+Route::prefix('pl')->as('pl.')->middleware(['restricted-ip'])->namespace('Poland')->group(function() {
     Route::post('/gus', 'GusController')->name('gus');
 });
